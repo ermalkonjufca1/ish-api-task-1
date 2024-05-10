b88bf6489d9ab81b93e536ea8c3e78557036c8e9
Ermal Konjufca <ermalkonjufca1@gmail.com>
2024-05-10 10:29:08 +0200

    solution for problem two

--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,39 @@ function solutionOne(): void {
   }
   person.leaveRoom();
 }
+
+function solutionTwo(): void {
+  const person = new Person();
+
+  const redLB = new LightBulb("red");
+  const greenLB = new LightBulb("green");
+  const blueLB = new LightBulb("blue");
+  const rooms = [new Room(greenLB), new Room(blueLB), new Room(redLB)];
+
+  rooms.forEach((room) => {
+    person.enterRoom(room);
+    person.switchLightBulb("ON");
+  });
+
+  setTimeout(() => {
+    const randomIndex = Math.floor(Math.random() * rooms.length);
+    rooms[randomIndex].lightBulb.break();
+  }, 10000);
+
+  setTimeout(() => {
+    rooms.forEach((room, i) => {
+      person.enterRoom(room);
+      if (person.currentRoom?.lightBulb.isLightBulbBroken()) {
+        console.log(`Light bulb on room (${i}) is broken, changing it`);
+        const newBulb = new LightBulb("red");
+        person.currentRoom.changeLightBulb(newBulb);
+      } else {
+        console.log(`Light bulb on room (${i}) is not broken`);
+      }
+      person.leaveRoom();
+    });
+  }, 60000);
+}
+
 // solutionOne();
+// solutionTwo();
